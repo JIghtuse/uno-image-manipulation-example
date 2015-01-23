f3688e6084467bff37072caa727ff59b4657782c
Boris Egorov <egorov@linux.com>
2015-01-23 17:24:47 +0600

    Add export_images option to ImageExample service
    Now when user press our new button in toolbar, all images from current
    document exported to /tmp folder with unique names like
    /tmp/be69d1af-dc8d-4ef1-a952-70080f7dca5d.png.

--- uno_image.py
+++ uno_image.py
@@ -5,7 +5,13 @@ Example usage of UNO, graphic objects and networking in LO extension
 import logging
 import uno
 import unohelper
+import uuid
+from com.sun.star.beans import PropertyValue
 from com.sun.star.task import XJobExecutor
+
+def generate_tmp_path():
+    """Generates unique temporary path to save image"""
+    return "/tmp/" + str(uuid.uuid4()) + ".png"
 
 class ImageExample(unohelper.Base, XJobExecutor):
     '''Class that implements the service registered in LibreOffice'''
@@ -31,11 +37,42 @@ class ImageExample(unohelper.Base, XJobExecutor):
             title,
             msg).execute()
 
+    def export_graphic(self, graphic, path):
+        """Exports graphic to path"""
+        logging.debug("exporting graphic {} to {}".format(graphic, path))
+
+        url = unohelper.systemPathToFileUrl(path)
+        props = ((PropertyValue("URL", 0, url, 0),
+                  PropertyValue("MimeType", 0, "image/" + path[-3:], 0),))
+
+        try:
+            self.graphics.storeGraphic(graphic, props)
+        except IOError as err:
+            self.show_warning("I/O exception", "I/O exception: {}".format(err))
+
     def trigger(self, args):
         """This method provides options for ImageExample service"""
-        
+
         if args == 'show_warning':
             self.show_warning("Warning", "Warning description here")
+        elif args == 'export_images':
+            component = self.desktop.CurrentComponent
+            graphics = component.getGraphicObjects()
+
+            if not graphics.hasElements():
+                self.show_warning("No images",
+                                  "This document contains no images. Nothing to do.")
+                return
+
+            for name in graphics.getElementNames():
+                props = graphics.getByName(name)
+
+                url = props.getPropertyValue("GraphicURL")
+                props = ((PropertyValue("URL", 0, url, 0)), )
+
+                graphic = self.graphics.queryGraphic(props)
+                self.export_graphic(graphic, generate_tmp_path())
+
 
 g_ImplementationHelper = unohelper.ImplementationHelper()
 g_ImplementationHelper.addImplementation(
